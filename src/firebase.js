c7510e253ac24931013322e3dcf21745e4d67094
erikavetisyan19 <135341606+erikavetisyan19@users.noreply.github.com>
2026-02-26 08:47:50 +0200

    Secure Firebase API keys by migrating to Environment Variables and updating gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDp7tefCAVKrZPA1UWpnAwIWD4SCt67j6o",
-    authDomain: "financialtracker-653a3.firebaseapp.com",
-    projectId: "financialtracker-653a3",
-    storageBucket: "financialtracker-653a3.firebasestorage.app",
-    messagingSenderId: "892807839835",
-    appId: "1:892807839835:web:ad7293d4743c068e6a3f7e"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
